7cc1871c8783aa54a81dc22256d4c8a76b417272
Mikela Clemmons <glassresistor@gmail.com>
2017-03-16 11:17:04 -0700

    migrated to python3

--- README.rst
+++ README.rst
@@ -1,5 +1,5 @@
 ================================================================================
-boto rsync v0.8.1
+boto rsync3 v0.8.1
 ================================================================================
 
 | Copyright: (c) 2012 Seth Davis
@@ -19,29 +19,12 @@ options). If the file exists on the destination but its size differs from
 the source, then it will be overwritten (unless the -w option is used).
 
 
-Installation
-================================================================================
-
-To install, simply::
-
-    pip install boto_rsync
-
-* You'll need to have `Python`_ 2.5+ and `pip`_ installed.
-* You might have to be root (or use sudo) for pip to install the script into a
-  globally executable directory in your $PATH.
-* pip should automatically install boto for you, but the advanced user can find
-  it here: http://github.com/boto/boto/
-
-.. _Python: http://www.python.org
-.. _pip: http://www.pip-installer.org
-
-
 Usage
 ================================================================================
 
 ::
 
-    boto-rsync [OPTIONS] SOURCE DESTINATION
+    boto-rsync3 [OPTIONS] SOURCE DESTINATION
 
 SOURCE and DESTINATION can be:
 
@@ -57,15 +40,15 @@ Examples
 
 ::
 
-    boto-rsync [OPTIONS] /local/path/ s3://bucketname/remote/path/
+    boto-rsync3 [OPTIONS] /local/path/ s3://bucketname/remote/path/
 
 or::
 
-    boto-rsync [OPTIONS] gs://bucketname/remote/path/or/key /local/path/
+    boto-rsync3 [OPTIONS] gs://bucketname/remote/path/or/key /local/path/
 
 or::
 
-    boto-rsync [OPTIONS] s3://bucketname/ s3://another_bucket/
+    boto-rsync3 [OPTIONS] s3://bucketname/ s3://another_bucket/
 
 
 Options
--- setup.py
+++ setup.py
@@ -1,16 +1,7 @@
 import os, sys
-try:
-    from distutils.core import setup
-except ImportError:
-    from setuptools import setup
+from setuptools import find_packages, setup
 
 install_requires = ['boto>=2.2.1']
-
-if sys.version_info[0] >= 3 or sys.version_info[:2] < (2, 5):
-    raise RuntimeError('Requires Python 2.5 or above and does not support '
-                       'Python 3')
-elif sys.version_info[:2] < (2, 7):
-    install_requires.append('argparse')
 
 here = os.path.abspath(os.path.dirname(__file__))
 try:
@@ -24,18 +15,18 @@ except IOError:
     CHANGES = ''
 
 setup(
-    name='boto_rsync',
+    name='boto_rsync3',
     version='0.8.1',
     author='Seth Davis',
     author_email='seth@curiasolutions.com',
     description="An rsync-like wrapper for boto's S3 and Google Storage " + \
                 "interfaces.",
     long_description=README + '\n\n' + CHANGES,
-    url='http://github.com/seedifferently/boto_rsync',
-    keywords='boto amazon aws s3 gs google storage cloud sync rsync',
+    url='http://github.com/codeforamerica/boto_rsync3',
+    keywords='rsync for boto and python 3',
     packages=[],
     install_requires=install_requires,
-    scripts=['bin/boto-rsync'],
+    scripts=['bin/boto-rsync3'],
     license = "MIT",
     platforms = "Posix; MacOS X; Windows",
     classifiers=[
@@ -45,10 +36,6 @@ setup(
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Topic :: Utilities',
         'Topic :: System :: Archiving',
         'Topic :: System :: Archiving :: Backup',
